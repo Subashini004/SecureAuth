38a173e2e51801d2ab6c9f6abb9af9dce19703b1
Sadhur <111108668+Sadhurnithy@users.noreply.github.com>
2025-06-21 00:02:46 +0530

    Email Function Added

--- src/main/java/in/sadhur/SecureAuth/controller/ProfileController.java
+++ src/main/java/in/sadhur/SecureAuth/controller/ProfileController.java
@@ -2,6 +2,7 @@ package in.sadhur.SecureAuth.controller;
 
 import in.sadhur.SecureAuth.io.ProfileRequest;
 import in.sadhur.SecureAuth.io.ProfileResponse;
+import in.sadhur.SecureAuth.service.EmailService;
 import in.sadhur.SecureAuth.service.ProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ProfileController {
     private final ProfileService profileServiceObj;
+    private final EmailService emailServiceObj;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public ProfileResponse register(@Valid @RequestBody ProfileRequest request) {
         ProfileResponse response = profileServiceObj.createProfile(request);
-        // Welcome Email
+        emailServiceObj.sendWelcomeEmail(response.getEmail(), response.getName());
         return response;
     }
 
---
+++ src/main/java/in/sadhur/SecureAuth/service/EmailService.java
@@ -0,0 +1,25 @@
+package in.sadhur.SecureAuth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendWelcomeEmail(String toEmail, String name) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Welcome to SecureAuth!");
+        message.setText("Hello " + name + ",\n\nThanks for registering with us!\n\nRegards,\nSecureAuth Team");
+        mailSender.send(message);
+    }
+}
